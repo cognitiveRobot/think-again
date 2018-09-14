cea9588673ae1c71dc15417c7dbbe894ed521e3a
zHossain <cognitiverobot@gmail.com>
2018-09-15 00:27:03 +1200

    Register and Login route have been added, but not database or data logic yet

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, StringField, PasswordField, validators
 
 app = Flask(__name__)
 
+# Home route
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -10,6 +12,28 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+#Register Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+# Register route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    return render_template('register.html', form=form)
+
+# Login Route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
