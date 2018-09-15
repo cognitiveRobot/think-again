470b853dc8c152eec9ae591bb9898da34392f95d
zHossain <cognitiverobot@gmail.com>
2018-09-15 23:11:30 +1200

    POST - Registration has been done

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, logging, session
 from wtforms import Form, StringField, PasswordField, validators
+from flask_mysqldb import MySQL
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_USER'] = 'thinkagainuser'
+app.config['MYSQL_PASSWORD'] = 'Abc#123456'
 app.config['MYSQL_DB'] = 'thinkagain'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
@@ -39,6 +41,25 @@ class RegisterForm(Form):
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 # Login Route
